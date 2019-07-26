4b45903764265780c8a888e89e0bde2995ba1b11
Steve Kasica <kasica@alumni.ubc.ca>
2019-07-26 00:25:04 +0000

    Update for meeting

---
+++ lib/util.py
@@ -0,0 +1,31 @@
+import codecs
+from pdfannots import pdfannots
+import pandas as pd
+import os, glob, re
+
+def getCodes():
+    """Extract codes generated in the open-coding process from PDFs"""
+    rep = lambda s, n: [ s for i in range(n) ]
+    codec = codecs.lookup('cp1252')
+    data = pd.DataFrame(columns=['org', 'article', 'analysis', 'index', 'cell', 'code'])
+    code_re = r'\[([^\]]+)\]\s([A-za-z][^\n]+)\n?'  # ReExp for parsing my coding comments
+
+    ptrn = os.path.join('.', 'notebooks', '**', '**', '*.html.pdf')
+    for fn in glob.iglob(ptrn, recursive=False):
+        org, article, analysis = fn.split('/')[2:]
+        with open(fn, 'rb') as fobj:
+            annots, outlines = pdfannots.process_file(fobj, codec, False)
+        codes = []
+        for annot in annots:
+            if annot.contents != None:
+                codes += re.findall(code_re, annot.contents)
+        df = pd.DataFrame({
+            'org': rep(org, len(codes)),
+            'article': rep(article, len(codes)),
+            'analysis': rep(analysis[:-9], len(codes)),  # slice off file extension
+            'index': [ i for i in range(len(codes)) ],
+            'cell': [ c[0].strip() for c in codes ],
+            'code': [ c[1].strip().lower() for c in codes ]
+        })
+        data = data.append(df)
+    return data
