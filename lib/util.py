021a63a93da00c9997363c96f13f69e22ce9b446
Steve Kasica <kasica@alumni.ubc.ca>
2019-08-27 20:35:15 +0000

    First round of tree balancing
    Trying to reduce too detailed codes. Also updates some methodological
    stuff about the codebook

--- lib/util.py
+++ lib/util.py
@@ -1,7 +1,7 @@
 """
     lib/util.py
     ---------------------------------------------------------------------------------------
-    Some functions shared between Jupyter notebooks
+    Some functions shared between Jupyter notebooks.
 
 """
 
@@ -15,12 +15,12 @@ def getCodes():
     """Extract codes generated in the open-coding process from PDFs"""
     rep = lambda s, n: [ s for i in range(n) ]
     codec = codecs.lookup('cp1252')
-    data = pd.DataFrame(columns=['org', 'article', 'analysis', 'index', 'cell', 'code'])
+    data = pd.DataFrame(columns=['org', 'analysis', 'notebook', 'index', 'cell', 'code'])
     code_re = r'\[([^\]]+)\]\s([A-za-z][^\n]+)\n?'  # ReExp for parsing my coding comments
 
     ptrn = os.path.join('.', 'notebooks', '**', '**', '*.html.pdf')
     for fn in glob.iglob(ptrn, recursive=False):
-        org, article, analysis = fn.split('/')[2:]
+        org, analysis, notebook = fn.split('/')[2:]
         with open(fn, 'rb') as fobj:
             annots, outlines = pdfannots.process_file(fobj, codec, False)
         codes = []
@@ -29,8 +29,8 @@ def getCodes():
                 codes += re.findall(code_re, annot.contents)
         df = pd.DataFrame({
             'org': rep(org, len(codes)),
-            'article': rep(article, len(codes)),
-            'analysis': rep(analysis[:-9], len(codes)),  # slice off file extension
+            'analysis': rep(analysis, len(codes)),
+            'notebook': rep(notebook[:-9], len(codes)),  # slice off file extension
             'index': [ i for i in range(len(codes)) ],
             'cell': [ c[0].strip() for c in codes ],
             'code': [ c[1].strip().lower() for c in codes ]
